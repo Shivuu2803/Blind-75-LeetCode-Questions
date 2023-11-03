abf043fc4db57de206ece4b903e20757c8dc7c88
Shivuu2803 <94588491+Shivuu2803@users.noreply.github.com>
2023-11-03 23:59:05 +0530

    Update 12-Missing Number.cpp

--- 12-Missing Number.cpp
+++ 12-Missing Number.cpp
@@ -6,6 +6,7 @@ public:
         int ans = nums.size();
         for (int i = 0; i < nums.size(); i++) 
         {
+            //xor ans with nums[i] and then the result will get xor by i
             ans ^= nums[i] ^ i;
         }
         return ans;
